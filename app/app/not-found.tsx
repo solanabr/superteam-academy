b279f04436c6eaf817d53b32fb6c86a5f06e1795
elcharitas <jonathanirhodia@gmail.com>
2026-02-26 08:11:15 +0100

    feat: implement NotFoundPage with locale support and internationalization

--- app/app/not-found.tsx
+++ app/app/not-found.tsx
@@ -1,1 +1,12 @@
-export { default } from "./[locale]/not-found";
+import { NextIntlClientProvider } from "next-intl";
+import { default as NotFound } from "./[locale]/not-found";
+import { getLocale } from "next-intl/server";
+
+export default async function NotFoundPage() {
+	const locale = await getLocale();
+	return (
+		<NextIntlClientProvider locale={locale}>
+			<NotFound />
+		</NextIntlClientProvider>
+	);
+}
