b0d694bae38a3a2bd00b79a46ea181d4e5094049
Myles181 <cyrile450@gmail.com>
2026-03-03 00:02:00 +0100

    Fix: Build errors

--- onchain-academy/app/lib/sanity/client.ts
+++ onchain-academy/app/lib/sanity/client.ts
@@ -2,8 +2,8 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     apiVersion: "2024-03-01",
     useCdn: false, // Set to true for production usage
     token: process.env.SANITY_API_DEV_TOKEN,
