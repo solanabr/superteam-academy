a40e287d99b80255f1e98fe6ef9dcac1008dee57
peace-dapps <peace.dapps@gmail.com>
2026-03-02 23:42:02 +0100

    fix: standalone output to fix Vercel build errors

--- app/next.config.ts
+++ app/next.config.ts
@@ -8,6 +8,7 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "solana.com" },
