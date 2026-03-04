28bcdcd86a3f14437db29b8a5082f75aaf2ce194
denisthe12 <denissnims@gmail.com>
2026-03-04 17:15:31 +0500

    fix force-dynamic and vercel

--- app/src/app/api/achievements/route.ts
+++ app/src/app/api/achievements/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   // Получаем все ачивки, отсортированные по сложности или имени
   const achievements = await prisma.achievement.findMany({
